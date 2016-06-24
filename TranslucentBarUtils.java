75380dfe9d53db1da1c83e64fb639e1087af1a97
Jocoo <xzpnuist@gmail.com>
2016-06-24 23:47:45 +0800

    add set status bar transparent function

--- TranslucentBarUtils.java
+++ TranslucentBarUtils.java
@@ -1,8 +1,6 @@
-package com.jocoo.translucentbardemo;
+package com.jocoo.wavedemo;
 
-import android.annotation.TargetApi;
 import android.graphics.Color;
-import android.graphics.Rect;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,19 +34,42 @@ public class TranslucentBarUtils {
         }
     }
 
-    public static void setStatusBarTranslucent(Window window) {
+    public static void setStatusBarTransparent(Window window) {
+        setStatusBarTransparent(window, true, false);
+    }
+
+    public static void setStatusBarTransparent(Window window, boolean isFitSystemWindows) {
+        setStatusBarTransparent(window, isFitSystemWindows, false);
+    }
+
+    public static void setStatusBarTransparent(Window window, boolean isFitSystemWindows, boolean isLightStatusBar) {
         if (window == null) return;
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return;
 
-        // 1. add translucent flag
-        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // 1. set flags
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.setStatusBarColor(Color.TRANSPARENT);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (isLightStatusBar) {
+                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                }
+            }
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
 
         // 2. set fitSystemWindow
-        ViewGroup content = (ViewGroup) ((FrameLayout) window.findViewById(android.R.id.content)).getChildAt(0);
-        if (content != null) {
-            content.setFitsSystemWindows(true);
-            content.setClipToPadding(true);
+        if (isFitSystemWindows) {
+            ViewGroup content = (ViewGroup) ((FrameLayout) window.findViewById(android.R.id.content)).getChildAt(0);
+            if (content != null) {
+                content.setFitsSystemWindows(true);
+                content.setClipToPadding(true);
+            }
         }
     }
 
