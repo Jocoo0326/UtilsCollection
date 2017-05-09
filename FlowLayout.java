7f9e987873e4c6c098145f7a2701138622893533
xzpnuist <947986967@qq.com>
2017-05-09 17:09:58 +0800

    make translucent bar utils compatible

--- FlowLayout.java
+++ FlowLayout.java
@@ -1,28 +1,56 @@
-package com.yxtk.views;
+package com.gdmm.lib.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import com.gdmm.lib.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 流式布局
- * <p/>
  * Created by Jocoo on 2016/8/5.
  */
 public class FlowLayout extends ViewGroup {
+    private static final Alignment[] sAlignmentArray = {
+            Alignment.LEFT,
+            Alignment.CENTER,
+            Alignment.RIGHT
+    };
 
-    public FlowLayout(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    private Alignment mAlignment;
+    private List<Integer> mLineFirstList;
+    private int mVerticalSpacing;
+    private int mHorizontalSpacing;
+    private List<Integer> mLineWidth;
+    private List<Integer> mLineHeight;
+
+    public FlowLayout(Context context) {
+        this(context, null);
     }
 
     public FlowLayout(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public FlowLayout(Context context) {
-        this(context, null);
+    public FlowLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.FlowLayout);
+        int index = typedArray.getInt(R.styleable.FlowLayout_align, 0);
+        mAlignment = sAlignmentArray[index];
+        mHorizontalSpacing = typedArray.getDimensionPixelSize(
+                R.styleable.FlowLayout_horizontal_spacing, 0);
+        mVerticalSpacing = typedArray.getDimensionPixelSize(
+                R.styleable.FlowLayout_vertical_spacing, 0);
+        typedArray.recycle();
+        mLineFirstList = new ArrayList<>();
+        mLineWidth = new ArrayList<>();
+        mLineHeight = new ArrayList<>();
     }
 
     @Override
@@ -34,6 +62,7 @@ public class FlowLayout extends ViewGroup {
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
+        int widthAllowed = widthSize - getPaddingLeft() - getPaddingRight();
         int widthLayout = 0;
         int heightLayout = 0;
         int widthLine = 0;
@@ -41,7 +70,14 @@ public class FlowLayout extends ViewGroup {
         int widthChild;
         int heightChild;
 
+
         int count = getChildCount();
+        mLineFirstList.clear();
+        mLineWidth.clear();
+        mLineHeight.clear();
+        if (count > 0) {
+            mLineFirstList.add(0); // the first element is the first line's first element.
+        }
         View child;
         MarginLayoutParams params;
         for (int i = 0; i < count; i++) {
@@ -53,26 +89,56 @@ public class FlowLayout extends ViewGroup {
             widthChild = child.getMeasuredWidth() + params.leftMargin + params.rightMargin;
             heightChild = child.getMeasuredHeight() + params.topMargin + params.bottomMargin;
 
-            if (widthLine + widthChild <= widthSize - getPaddingLeft() - getPaddingRight()) {
-                widthLine += widthChild;
-                heightLine = Math.max(heightLine, heightChild);
-            } else {
+            if (widthChild > widthAllowed) continue;
+
+            if (i > 0 && widthLine + widthChild + mHorizontalSpacing > widthAllowed) {
+                mLineWidth.add(widthLine);
+                mLineHeight.add(heightLine);
                 widthLayout = Math.max(widthLayout, widthLine);
-                heightLayout += heightLine;
+                heightLayout += heightLine + mVerticalSpacing;
                 widthLine = widthChild;
                 heightLine = heightChild;
+                mLineFirstList.add(i);
+            } else {
+                widthLine += widthChild + (i == 0 ? 0 : mHorizontalSpacing);
+                heightLine = Math.max(heightLine, heightChild);
             }
             child.setLeft(getPaddingLeft() + widthLine - params.rightMargin - child.getMeasuredWidth());
             child.setTop(getPaddingTop() + heightLayout + params.topMargin);
             if (i == count - 1) {
+                mLineWidth.add(widthLine);
+                mLineHeight.add(heightLine);
                 widthLayout = Math.max(widthLayout, widthLine);
                 heightLayout += heightLine;
             }
         }
         setMeasuredDimension(
-            widthMode == MeasureSpec.EXACTLY ? widthSize : widthLayout + getPaddingLeft() + getPaddingRight(),
-            heightMode == MeasureSpec.EXACTLY ? heightSize : heightLayout + getPaddingLeft() + getPaddingRight()
+                widthMode == MeasureSpec.EXACTLY ? widthSize : widthLayout + getPaddingLeft() + getPaddingRight(),
+                heightMode == MeasureSpec.EXACTLY ? heightSize : heightLayout + getPaddingTop() + getPaddingBottom()
         );
+
+        // set the alignment
+        int lineCount = mLineWidth.size();
+        if (lineCount != mLineFirstList.size()) return;
+        for (int i = 0; i < lineCount; i++) {
+            int lastIndexOfLine = (i < lineCount - 1) ? mLineFirstList.get(i + 1) : count;
+            for (int j = mLineFirstList.get(i); j < lastIndexOfLine; j++) {
+                int offsetX = 0;
+                if (mAlignment == Alignment.LEFT) {
+                    offsetX = 0;
+                } else if (mAlignment == Alignment.CENTER) {
+                    offsetX = (widthLayout - mLineWidth.get(i)) / 2;
+                } else if (mAlignment == Alignment.RIGHT) {
+                    offsetX = widthLayout - mLineWidth.get(i);
+                }
+                child = getChildAt(j);
+                child.setLeft(child.getLeft() + offsetX);
+            }
+        }
+    }
+
+    public int getFirstLineHeight() {
+        return mLineHeight.size() > 0 ? mLineHeight.get(0) : 0;
     }
 
     @Override
@@ -81,10 +147,10 @@ public class FlowLayout extends ViewGroup {
         for (int i = 0; i < getChildCount(); i++) {
             child = getChildAt(i);
             child.layout(
-                child.getLeft(),
-                child.getTop(),
-                child.getLeft() + child.getMeasuredWidth(),
-                child.getTop() + child.getMeasuredHeight()
+                    child.getLeft(),
+                    child.getTop(),
+                    child.getLeft() + child.getMeasuredWidth(),
+                    child.getTop() + child.getMeasuredHeight()
             );
         }
     }
@@ -95,5 +161,17 @@ public class FlowLayout extends ViewGroup {
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    public enum Alignment {
+        LEFT(0),
+        CENTER(1),
+        RIGHT(2);
+
+        Alignment(int ni) {
+            this.nativeInt = ni;
+        }
+
+        final int nativeInt;
     }
 }
--- TranslucentBarUtils.java
+++ TranslucentBarUtils.java
@@ -1,7 +1,12 @@
-package com.jocoo.wavedemo;
+package com.jocoo.daggerdemo.util;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -48,29 +53,35 @@ public class TranslucentBarUtils {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return;
 
         // 1. set flags
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.setStatusBarColor(Color.TRANSPARENT);
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (isLightStatusBar) {
-                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        if (isLightStatusBar && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                if (hasNavBar(window)) {
+                    window.getDecorView().setPadding(0, 0, 0, getNavBarHeight(window.getContext()));
                 }
-            }
         } else {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else {
+            window.setStatusBarColor(Color.TRANSPARENT);
         }
 
         // 2. set fitSystemWindow
-        if (isFitSystemWindows) {
+        if (isFitSystemWindows)
+        {
             ViewGroup content = (ViewGroup) ((FrameLayout) window.findViewById(android.R.id.content)).getChildAt(0);
             if (content != null) {
                 content.setFitsSystemWindows(true);
                 content.setClipToPadding(true);
             }
         }
+
     }
 
     private static View createStatusView(Window window, int color) {
@@ -88,5 +99,33 @@ public class TranslucentBarUtils {
         statusView.setLayoutParams(params);
         statusView.setBackgroundColor(color);
         return statusView;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public static boolean hasNavBar(Window window) {
+        Display d = window.getWindowManager().getDefaultDisplay();
+
+        DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+        d.getRealMetrics(realDisplayMetrics);
+
+        int realHeight = realDisplayMetrics.heightPixels;
+        int realWidth = realDisplayMetrics.widthPixels;
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        d.getMetrics(displayMetrics);
+
+        int displayHeight = displayMetrics.heightPixels;
+        int displayWidth = displayMetrics.widthPixels;
+
+        return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
+    }
+
+    public static int getNavBarHeight(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return resources.getDimensionPixelSize(resourceId);
+        }
+        return 0;
     }
 }
